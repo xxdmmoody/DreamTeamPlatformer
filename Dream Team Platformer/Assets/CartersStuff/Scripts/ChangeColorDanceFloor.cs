a316772616cabad393359391ce9ce8aaa6e45d81
Carter Hunt <realife100@gmail.com>
2018-10-08 18:29:01 -0500

    Carters stuff added

---
+++ Dream Team Platformer/Assets/CartersStuff/Scripts/ChangeColorDanceFloor.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorDanceFloor : MonoBehaviour {
+
+    float startTime;
+    float elapsedTime;
+    float time;
+    public Material greyDanceFloor;
+    public GameObject ChangeMyColor;
+    public GameObject[] Patterns;
+    public GameObject[] NOTpatterns;
+    
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        elapsedTime = Time.time - startTime;
+        //time = Time.timeSinceLevelLoad;
+
+        if (elapsedTime >= 10f)
+        {
+            gameObject.GetComponent<MeshRenderer>().materials[0].color = Color.grey;
+
+        }
+        if((Patterns[0].GetComponent<MeshRenderer>().materials[0].color == Color.green &&
+            Patterns[1].GetComponent<MeshRenderer>().materials[0].color == Color.green &&
+            Patterns[2].GetComponent<MeshRenderer>().materials[0].color == Color.green &&
+            Patterns[3].GetComponent<MeshRenderer>().materials[0].color == Color.green &&
+            Patterns[4].GetComponent<MeshRenderer>().materials[0].color == Color.green &&
+            Patterns[5].GetComponent<MeshRenderer>().materials[0].color == Color.green &&
+            Patterns[6].GetComponent<MeshRenderer>().materials[0].color == Color.green &&
+            Patterns[7].GetComponent<MeshRenderer>().materials[0].color == Color.green &&
+            Patterns[8].GetComponent<MeshRenderer>().materials[0].color == Color.green) &&
+            (NOTpatterns[0].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[1].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[2].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[3].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[4].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[5].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[6].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[7].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[8].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[9].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[10].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[11].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[12].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[13].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[14].GetComponent<MeshRenderer>().materials[0].color == Color.gray &&
+            NOTpatterns[15].GetComponent<MeshRenderer>().materials[0].color == Color.gray
+            )  )
+        
+        {
+            ChangeMyColor.GetComponent<MeshRenderer>().materials[0].color = Color.green;
+        }
+
+
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.CompareTag("Player") && gameObject.GetComponent<MeshRenderer>().materials[0].color != Color.green)
+        {
+            gameObject.GetComponent<MeshRenderer>().materials[0].color = Color.green;
+            startTime = Time.time;
+        }
+    }
+
+}
---
+++ Dream Team Platformer/Assets/CartersStuff/Scripts/Standard Assets/Characters/FirstPersonCharacter/Scripts/GenerateLevel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour {
+    public float distanceToInstantiate = 100;
+    public GameObject basePlatform;
+    public GameObject obstacle;
+
+    // Use this for initialization
+    void Start()
+    {
+       /* Vector3 currentPosition = Vector3.zero;
+        Quaternion rotation = Quaternion.Euler(0, 0, 0);
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(basePlatform, currentPosition, rotation, gameObject.transform);
+            currentPosition += new Vector3(0, 0, 12f);
+        }*/
+    }
+    Vector3 currentPosition = Vector3.zero;
+    Vector3 currentPosition2 = Vector3.zero;
+    int x, y, z;
+    Quaternion rotation = Quaternion.Euler(0, 0, 0);
+    // Update is called once per frame
+    void Update () {
+        if (Vector3.Distance(Camera.main.transform.position, currentPosition) < distanceToInstantiate)
+        {
+            Input.GetAxis("Fire1");
+            Instantiate(basePlatform, currentPosition, rotation, gameObject.transform);
+            x = Random.Range(-2, 3);
+            y = Random.Range(-1, 2);
+            z = 12;
+            currentPosition += new Vector3(x, y, z);
+            currentPosition2 += new Vector3(x, y, z);
+            Instantiate(obstacle, currentPosition2 + new Vector3(Random.Range(-1, 1), Random.Range(1, 2), 12), rotation, gameObject.transform);
+           // currentPosition2 += new Vector3(Random.Range(-2, 3), Random.Range(-1, 2), 12f);
+        }
+    }
+}
